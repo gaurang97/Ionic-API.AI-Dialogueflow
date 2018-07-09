0493497bddb31f48602a7a1ad3a0be6c4f715076
Gaurang Dadheech <gaurangdadheech@gmail.com>
2018-07-09 18:51:04 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, NgZone } from '@angular/core';
 
+import { Platform, NavController } from 'ionic-angular';
+import {ApiAiClient} from "api-ai-javascript";
+const client = new ApiAiClient({accessToken: '75b05e7e522548ecb9d52428522944fe'})
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  answer:any;
+  constructor(public navCtrl: NavController,public ngZone: NgZone, public platform: Platform) {
+    
   }
-
+ ask(question){
+   client.textRequest(question)
+   .then(({result: {fulfillment : {speech}}})=>
+  this.ngZone.run(()=>{
+    this.answer = speech;
+  }))
+ }
 }
